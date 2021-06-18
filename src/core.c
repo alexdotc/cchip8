e2a02ee900cb9d6745dbfafd19a641839ec2a541
Alex Chavkin <alexlchavkin@gmail.com>
2021-06-18 14:33:43 -0400

    Better error handling

--- src/core.c
+++ src/core.c
@@ -5,47 +5,41 @@
 #define SPEED 600 // hz
 #define SCALE 20 // 64x32 multiplier
 
-Options *parse_options(int argc, char *const argv[]);
+static Options *parse_options(int argc, char *const argv[]);
+static inline int fatal_error(const char *msg);
 
 int main(int argc, char *const argv[])
 {
     Options *options = parse_options(argc, argv);
-    if (options == NULL) {
-        fprintf(stderr, "Failed to alloc options struct. Exiting...\n");
-        return EXIT_FAILURE;
-    }
-    if (options->speed < 60){
-        fprintf(stderr, "CPU speed must be at least 60hz. Exiting...\n");
-        return EXIT_FAILURE;
-    }
-    if (options->rom == NULL) {
-        fprintf(stderr, "No ROM provided. Exiting...\n");
-        return EXIT_FAILURE;
-    }
+    if (options == NULL) return fatal_error("Failed to alloc options struct.");
+    if (options->speed < 60) return fatal_error("CPU speed must be at least 60hz");
+    if (options->scale < 1) return fatal_error("Window scale must be at least 1.");
+    if (options->rom == NULL) return fatal_error("No ROM provided.");
 
     init_platform(options);
 
     Chip8 chip8 = { MEMSIZE, GFXSIZE, STACKSIZE };
 
     reset(&chip8);
-    load_rom(&chip8, options->rom);
+    int lr = load_rom(&chip8, options->rom);
+    if (lr != 0) return fatal_error("Could not load ROM at the given location.");
+
     uint64_t ctr = 0; // shouldn't overflow at intended range of speeds
 
     while(ctr += 1)
     {
         cycle(&chip8);
         if ( ! (ctr % (SPEED/60))) dec_timers(&chip8); // approximate timing
-        if (update_platform(&chip8) < 0) goto quit;
+        if (update_platform(&chip8) < 0) break;
         nanosleep((const struct timespec[]){{0, 1000000000L/options->speed}}, NULL);
     }
 
-    quit:
     free(options);
     
     return 0;
 }
 
-Options *parse_options(int argc, char *const argv[])
+static Options *parse_options(int argc, char *const argv[])
 {
     int opt;
     Options *options = malloc(sizeof(Options));
@@ -64,4 +58,10 @@ Options *parse_options(int argc, char *const argv[])
     if (optind < argc) options->rom = argv[optind++];
 
     return options;
+}
+
+static inline int fatal_error(const char *msg)
+{
+    fprintf(stderr, "%s Exiting...\n", msg);
+    return EXIT_FAILURE;
 }
